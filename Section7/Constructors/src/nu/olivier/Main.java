e0efc554d1cef180299e61867ae8336685a3cf9b
Stephan Olivier <sharpie.lovenit@gmail.com>
2017-07-16 19:10:37 +0200

    initial commit

---
+++ Section7/Constructors/src/nu/olivier/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VipCustomer Default = new VipCustomer();
+        System.out.println(Default.toString());
+
+        VipCustomer Stephan = new VipCustomer("John", "john@doe.com");
+        System.out.println(Stephan.toString());
+
+        VipCustomer Mike = new VipCustomer("Mike", "mike@test.com", 200);
+        System.out.println(Mike.toString());
+    }
+}
---
+++ Section7/Constructors/src/nu/olivier/VipCustomer.java
@@ -0,0 +1,44 @@
+package com.company;
+
+/**
+ * Created by Stephan.Olivier on 07/16/2017.
+ */
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer () {
+        this("Default", "default@default.com", 10);
+    }
+
+    public VipCustomer (String name, String email) {
+        this(name, email, 100 );
+    }
+
+    public VipCustomer (String name, String email, double creditLimit) {
+        this.name = name;
+        this.email = email;
+        this.creditLimit = creditLimit;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "VipCustomer{" +
+                "name='" + name + '\'' +
+                ", creditLimit=" + creditLimit +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
