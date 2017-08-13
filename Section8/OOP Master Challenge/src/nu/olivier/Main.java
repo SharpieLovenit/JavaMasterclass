802a86b3833841b68892cb66e4536bfc675b4dc1
Stephan Olivier <sharpie.lovenit@gmail.com>
2017-08-13 21:06:33 +0200

    initial commit

---
+++ Section8/OOP Master Challenge/src/nu/olivier/Main.java
@@ -0,0 +1,13 @@
+package nu.olivier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizedHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total burger price is: " + hamburger.itemizedHamburger());
+    }
+}
