f0cee6a8ece08b0219ba2a380eca122365821a76
Stephan Olivier <sharpie.lovenit@gmail.com>
2017-08-13 21:30:47 +0200

    added healty and deluxe burgers

--- Section8/OOP Master Challenge/src/nu/olivier/Main.java
+++ Section8/OOP Master Challenge/src/nu/olivier/Main.java
@@ -9,5 +9,14 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.13);
         System.out.println("Total burger price is: " + hamburger.itemizedHamburger());
+
+        HealthyBurger healty = new HealthyBurger("Bacon", 5.67);
+        healty.addHamburgerAddition1("Egg", 2);
+        healty.addHealthAddition1("Lentils", 0.50);
+        System.out.println("Total Healty burger price is: " + healty.itemizedHamburger());
+
+        DeluxeBurger deluxe = new DeluxeBurger();
+        deluxe.addHamburgerAddition1("should not work", 1);
+        deluxe.itemizedHamburger();
     }
 }
